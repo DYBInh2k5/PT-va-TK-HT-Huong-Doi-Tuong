eef8a0a4ca1b39fe7650d18d1c22f89fed15bc7f
Võ Duy Bình <binh.vd01500@sinhvien.hoasen.edu.vn>
2025-11-27 13:47:10 +0700

    Add files via upload

---
+++ StudentDemo/src/studentdemo/StudentDemo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentdemo;
+
+/**
+ *
+ * @author User
+ */
+public class StudentDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFStudent jfs = new JFStudent();
+
+        jfs.show();
+    }
+    
+}
